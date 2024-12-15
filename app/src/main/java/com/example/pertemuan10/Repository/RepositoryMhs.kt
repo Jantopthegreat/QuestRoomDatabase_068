08fa19ce4089299f6574bcfa2d0621470cc7302e
Jantopthegreat <fauzangriya@gmail.com>
2024-12-16 05:23:06 +0700

    memanggil funsgi getAllMhs dari MahasiswaDao

--- app/src/main/java/com/example/pertemuan10/Repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/Repository/RepositoryMhs.kt
@@ -1,7 +1,11 @@
 package com.example.pertemuan10.Repository
 
 import com.example.pertemuan10.Data.Entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
 }
