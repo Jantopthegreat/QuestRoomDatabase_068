a238b9f901e76ee2878967578aba8c7bc779bc40
Jantopthegreat <fauzangriya@gmail.com>
2024-12-16 06:39:06 +0700

    Membuat fungsi deleteMahasiswa di detailUiState

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/DetailMhsViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 
 class DetailMhsViewModel (
@@ -51,26 +52,35 @@ class DetailMhsViewModel (
                 isLoading = true,
             ),
         )
-data class DetailUiState(
-    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
-    val isLoading: Boolean = false,
-    val isError: Boolean = false,
-    val errorMessage: String = ""
-) {
-    val isUiEventEmpty: Boolean
-        get() = detailUiEvent == MahasiswaEvent()
-
-    val isUiEventNotEmpty: Boolean
-        get() = detailUiEvent != MahasiswaEvent()
+    fun deleteMhs() {
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMahasiswa(it)
+            }
+        }
+    }
 }
 
-fun Mahasiswa.toDetailUiEvent() : MahasiswaEvent {
-    return MahasiswaEvent(
-        nim = nim,
-        nama = nama,
-        jenisKelamin = jenisKelamin,
-        alamat = alamat,
-        kelas = kelas,
-        angkatan = angkatan
-    )
-}}
+    data class DetailUiState(
+        val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+        val isLoading: Boolean = false,
+        val isError: Boolean = false,
+        val errorMessage: String = ""
+    ) {
+        val isUiEventEmpty: Boolean
+            get() = detailUiEvent == MahasiswaEvent()
+
+        val isUiEventNotEmpty: Boolean
+            get() = detailUiEvent != MahasiswaEvent()
+    }
+
+    fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
+        return MahasiswaEvent(
+            nim = nim,
+            nama = nama,
+            jenisKelamin = jenisKelamin,
+            alamat = alamat,
+            kelas = kelas,
+            angkatan = angkatan
+        )
+    }
