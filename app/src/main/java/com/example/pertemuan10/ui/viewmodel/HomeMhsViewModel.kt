826d4b4f419e66ead9e2887c0fc99de35d6f1940
Jantopthegreat <fauzangriya@gmail.com>
2024-12-16 06:13:00 +0700

    mengimport kotlin coroutine dan membuat class data HomeUiState

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
@@ -2,10 +2,16 @@ package com.example.pertemuan10.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pertemuan10.Data.Entity.Mahasiswa
 import com.example.pertemuan10.Repository.RepositoryMhs
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel ( private val repositoryMhs : RepositoryMhs) : ViewModel() {
 
@@ -38,3 +44,10 @@ class HomeMhsViewModel ( private val repositoryMhs : RepositoryMhs) : ViewModel(
             )
         )
 }
+
+data class HomeUiState (
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
