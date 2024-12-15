268de84a234351199f1114bb7f45089352f7e761
Jantopthegreat <fauzangriya@gmail.com>
2024-12-16 05:19:56 +0700

    fungsi untuk mengupdate data mahasiswa

--- app/src/main/java/com/example/pertemuan10/Data/Dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan10/Data/Dao/MahasiswaDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.pertemuan10.Data.Entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -22,6 +23,9 @@ interface MahasiswaDao {
 
     @Delete
     suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa (mahasiswa: Mahasiswa)
 
 }
 
