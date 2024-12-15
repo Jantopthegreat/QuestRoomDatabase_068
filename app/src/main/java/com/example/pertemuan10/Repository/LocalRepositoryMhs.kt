80b94b7e89b422e14ae3e4061f39259523958f80
Jantopthegreat <fauzangriya@gmail.com>
2024-12-16 05:37:01 +0700

    memanggil fungsi getAllMahasiswa dari MahasiswaDao untuk menampilkan semua data mahasiswa, dan memperbaiki penamaan di RepositoryMhs

--- app/src/main/java/com/example/pertemuan10/Repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/Repository/LocalRepositoryMhs.kt
@@ -2,11 +2,17 @@ package com.example.pertemuan10.Repository
 
 import com.example.pertemuan10.Data.Dao.MahasiswaDao
 import com.example.pertemuan10.Data.Entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(private val mahasiswaDao: MahasiswaDao) : RepositoryMhs {
 
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+    override fun getAllMahasiswa(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
 
 }
--- app/src/main/java/com/example/pertemuan10/Repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/Repository/RepositoryMhs.kt
@@ -6,12 +6,12 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
 
-    fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 
-    fun getMhs(nim: String): Flow<Mahasiswa>
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
 
-    suspend fun  deleteMhs (mahasiswa: Mahasiswa)
+    suspend fun  deleteMahasiswa (mahasiswa: Mahasiswa)
 
-    suspend fun updateMhs (mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa (mahasiswa: Mahasiswa)
 
 }
