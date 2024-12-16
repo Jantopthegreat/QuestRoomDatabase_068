84848900f735ef701bc28d10e6378eab9ed081df
Jantopthegreat <fauzangriya@gmail.com>
2024-12-17 02:43:40 +0700

    menambahkan fungsi DestinasiDetail yang sudah dibuat berdasarkan nim di page Home

--- app/src/main/java/com/example/pertemuan10/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan10/ui/navigation/PengelolaHalaman.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.pertemuan10.ui.view.mahasiswa.DestinasiInsert
+import com.example.pertemuan10.ui.view.mahasiswa.HomeMhsView
 import com.example.pertemuan10.ui.view.mahasiswa.insertMhsView
 
 
@@ -19,7 +20,21 @@ fun PengelolaHalaman(
     NavHost (
         navController = navController,
         startDestination = DestinasiHome.route
-    ){
+    ) {
         composable(
             route = DestinasiHome.route
-        ) {}
+        ) {
+            HomeMhsView(
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println("PengelolaHalaman: nim = $nim")
+                },
+                onAddMhs = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                modifier = modifier
+            )
+        }
+
+    }
+}
